ee0b6c6f8876e48406d43ae416f82b08ef17323e
“Freddie <freddielev@gmail.com>
2018-11-01 13:42:58 -0600

    Initial Commit, add Beer prize data

---
+++ BeerData.js
@@ -0,0 +1,603 @@
+const beerData = [
+
+{
+  beerName: 'Vanilla Bean Assassin',
+  brewery: 'Toppling Goliath'
+  year: 2013,
+  value: 5000
+},
+{
+  beerName: 'Driesjes Eerste Plasje',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 1997,
+  value: 3000
+},
+{
+  beerName: 'Bourgogne Lambic',
+  brewery: 'Brasserie Cantillon',
+  year: 2005,
+  value: 3000
+},
+{
+  beerName: 'Don Quijote',
+  brewery: 'Brasserie Cantillon',
+  year: 2008,
+  value: 3000
+},
+{
+  beerName: 'Dave',
+  brewery: 'Hair of the Dog Brewing CO',
+  year: 1994,
+  value: 2300
+},
+{
+  beerName: 'Loerik',
+  brewery: 'Brasserie Cantillon',
+  year: 2004,
+  value: 2200
+},
+{
+  beerName: 'Pikkulnnun Viskiambic',
+  brewery: 'Brasserie Cantillon',
+  year: 2009,
+  value: 1850
+},
+{
+  beerName: 'Morning Delight Batch 1',
+  brewery: 'Toppling Goliath',
+  year: 2013,
+  value: 1750
+},
+{
+  beerName: 'La Vie est Belge',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2016,
+  value: 1725
+},
+{
+  beerName: 'Summation',
+  brewery: '3 Sons Brewing Co',
+  year: 2015,
+  value: 1600
+},
+{
+  beerName: 'Jean Chris Nomad',
+  brewery: 'Brasserie Cantillon',
+  year: 2011,
+  value: 1500
+},
+{
+  beerName: 'J & J Roze',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2002,
+  value: 1500
+},
+{
+  beerName: 'Cable Car Kriek',
+  brewery: 'Lost Abbey',
+  year: 2011,
+  value: 1500
+},
+{
+  beerName: 'Kentucky Brunch Brand Stout Batch 1',
+  brewery: 'Toppling Goliath',
+  year: 2013,
+  value: 1500
+},
+{
+  beerName: 'Iris',
+  brewery: 'Brasserie Cantillon',
+  year: 1996,
+  value: 1400
+},
+{
+  beerName: 'J & J Blauw',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2003,
+  value: 1400
+},
+{
+  beerName: 'Millennium Gueuze',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2000,
+  value: 1290
+},
+{
+  beerName: 'Ann',
+  brewery: 'Hill Farmstead',
+  year: 2015,
+  value: 1150
+},
+{
+  beerName: 'Brabantiae',
+  brewery: 'Brasserie Cantillon',
+  year: 1990,
+  value: 1106
+},
+{
+  beerName: 'Black Raspberry Lambic',
+  brewery: 'Southampton Publick House',
+  year: 2012,
+  value: 1100
+},
+{
+  beerName: 'Eszett Geuze',
+  brewery: 'Braustelle',
+  year: 2010,
+  value: 1100
+},
+{
+  beerName: 'Derivation #2',
+  brewery: 'Side Project',
+  year: 2015,
+  value: 1100
+},
+{
+  beerName: 'Kentucky Brunch Brand Stout Batch 2',
+  brewery: 'Toppling Goliath',
+  year: 2014,
+  value: 1100
+},
+{
+  beerName: 'Veritas 005',
+  brewery: 'Lost Abbey',
+  year: 2009,
+  value: 1030
+},
+{
+  beerName: "Armand'4 Set Lente, Winter, Herfst, Zomer",
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2011,
+  value: 1025
+},
+{
+  beerName: 'Framboos',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 1999,
+  value: 1000
+},
+{
+  beerName: 'Malvasia Rosso',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2003,
+  value: 1000
+},
+{
+  beerName: 'SR-71',
+  brewery: 'Toppling Goliath',
+  year: 2015,
+  value: 1000
+},
+{
+  beerName: 'Brandy Vanilla Dark Lord',
+  brewery: 'Three Floyds',
+  year: 2011,
+  value: 999
+},
+{
+  beerName: 'Framboise',
+  brewery: 'Brasserie Cantillon',
+  year: 1982,
+  value: 950
+},
+{
+  beerName: 'Rare Scooop',
+  brewery: 'Cycle Brewing',
+  year: 2016,
+  value: 950
+},
+{
+  beerName: 'Kentucky Brand Brunch Stout Batch 3',
+  brewery: 'Toppling Goliath',
+  year: 2017,
+  value: 925
+},
+{
+  beerName: 'Don Quijote',
+  brewery: 'Brasserie Cantillon',
+  year: 2008,
+  value: 900
+},
+{
+  beerName: 'Lou Pepe Gueuze',
+  brewery: 'Brasserie Cantillon',
+  year: 1999,
+  value: 861
+},
+{
+  beerName: 'Crianza Helena',
+  brewery: 'Brasserie Cantillon',
+  year: 2008,
+  value: 850
+},
+{
+  beerName: 'Beatification',
+  brewery: 'Russian River',
+  year: 2013,
+  value: 850
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord (Marshmallow Handjee)',
+  brewery: '3 Floyds',
+  year: 2016,
+  value: 800
+},
+{
+  beerName: 'Derivation #6',
+  brewery: 'Side Project',
+  year: 2017,
+  value: 800
+},
+{
+  beerName: 'Lou Pepe Kriek Sticker Date 1998',
+  brewery: 'Brasserie Cantillon',
+  year: 2000,
+  value: 799
+},
+{
+  beerName: 'Heerengeuze',
+  brewery: 'Brasserie Cantillon',
+  year: 2011,
+  value: 799
+},
+{
+  beerName: 'Ann',
+  brewery: 'Hill Farmstead',
+  year: 2012,
+  value: 750
+},
+{
+  beerName: 'Lou Pepe Kriek',
+  brewery: 'Brasserie Cantillon',
+  year: 1999,
+  value: 750
+},
+{
+  beerName: 'Oude Gueuze',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 1998,
+  value: 750
+},
+{
+  beerName: 'M',
+  brewery: 'Midnight Sun Brewing',
+  year: 2005,
+  value: 750
+},
+{
+  beerName: 'MMM',
+  brewery: 'Sam Adams',
+  year: 1999,
+  value: 699
+},
+{
+  beerName: 'Assassin Batch 2',
+  brewery: 'Toppling Goliath',
+  year: 2012,
+  value: 699
+},
+{
+  beerName: 'Vigneronne',
+  brewery: 'Brasserie Cantillon',
+  year: 1996,
+  value: 660
+},
+{
+  beerName: "Lambic d'Aunis",
+  brewery: 'Brasserie Cantillon',
+  year: 2015,
+  value: 655
+},
+{
+  beerName: 'Gueuze',
+  brewery: 'Brasserie Cantillon',
+  year: 1981,
+  value: 651
+},
+{
+  beerName: 'Oude Fermier',
+  brewery: 'Side Project',
+  year: 2014,
+  value: 650
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord',
+  brewery: '3 Floyds',
+  year: 2014,
+  value: 650
+},
+{
+  beerName: 'Barrel Aged Mexican Cake Set',
+  brewery: 'Westbrook Brewing CO',
+  year: 2013,
+  value: 650
+},
+{
+  beerName: 'Gueuze',
+  brewery: 'Brasserie Cantillon',
+  year: 1988,
+  value: 650
+},
+{
+  beerName: 'Lou Pepe Gueuze',
+  brewery: 'Brasserie Cantillon',
+  year: 1998,
+  value: 650
+},
+{
+  beerName: 'Rue The Crown',
+  brewery: 'Toppling Goliath',
+  year: 2016,
+  value: 650
+},
+{
+  beerName: 'Assassin Yellow Wax',
+  brewery: 'Toppling Goliath',
+  year: 2013,
+  value: 650
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord',
+  brewery: '3 Floyds',
+  year: 2012,
+  value: 629
+},
+{
+  beerName: 'Blabaer Batch 1',
+  brewery: 'Brasserie Cantillon',
+  year: 2005,
+  value: 625
+},
+{
+  beerName: 'Framboise du Fermier',
+  brewery: 'Side Project',
+  year: 2015,
+  value: 600
+},
+{
+  beerName: 'Very Sour Blackberry',
+  brewery: 'New Glarus',
+  year: 2013,
+  value: 600
+},
+{
+  beerName: 'Bourbon Barrel Cherry Cocoa Dark Lord',
+  brewery: '3 Floyds',
+  year: 2015,
+  value: 600
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord',
+  brewery: '3 Floyds',
+  year: 2013,
+  value: 600
+},
+{
+  beerName: 'Double Barrel Damon',
+  brewery: 'Hill Farmstead',
+  year: 2013,
+  value: 600
+},
+{
+  beerName: 'Assassin Metallic Gold Wax',
+  brewery: 'Toppling Goliath',
+  year: 2014,
+  value: 600
+},
+{
+  beerName: 'Mimosa',
+  brewery: 'Hill Farmstead',
+  year: 2012,
+  value: 599.99
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord (Marshmallow Handjee)',
+  brewery: '3 Floyds',
+  year: 2017,
+  value: 575
+},
+{
+  beerName: 'Gueuze',
+  brewery: 'Brasserie Cantillon',
+  year: 1979,
+  value: 560
+},
+{
+  beerName: 'Oude Geuze 1999',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 1999,
+  value: 553
+},
+{
+  beerName: 'Oude Gueuze',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2004,
+  value: 550
+},
+{
+  beerName: 'Chez Monus',
+  brewery: 'Alpine',
+  year: 2013,
+  value: 550
+},
+{
+  beerName: 'Dark Lord de Muerte',
+  brewery: 'Hill Farmstead',
+  year: 2011,
+  value: 550
+},
+{
+  beerName: 'Derivation #1',
+  brewery: 'Side Project',
+  year: 2015,
+  value: 550
+},
+{
+  beerName: 'Bourbon Vanilla Dark Lord',
+  brewery: '3 Floyds',
+  year: 2015,
+  value: 549.99
+},
+{
+  beerName: 'Label Us Notorious',
+  brewery: 'J. Wakefield',
+  year: 2017,
+  value: 535
+},
+{
+  beerName: 'French Vanilla Militia Dark Lord',
+  brewery: '3 Floyds',
+  year: 2016,
+  value: 525
+},
+{
+  beerName: 'Oude Gueuze Vintage',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2002,
+  value: 525
+},
+{
+  beerName: 'Zwanze',
+  brewery: 'Brasserie Cantillon',
+  year: 2002,
+  value: 522.75
+},
+{
+  beerName: 'Blabaer Batch 10',
+  brewery: 'Brasserie Cantillon',
+  year: 2014,
+  value: 500
+},
+{
+  beerName: 'Blabaer Batch 11',
+  brewery: 'Brasserie Cantillon',
+  year: 2015,
+  value: 500
+},
+{
+  beerName: 'Darkness',
+  brewery: 'Surly Brewing',
+  year: 2007,
+  value: 500
+},
+{
+  beerName: 'Reine des Pres',
+  brewery: 'Brasserie Cantillon',
+  year: 2017,
+  value: 500
+},
+{
+  beerName: 'Bourbon Dark Lord Pappy Van Winkle',
+  brewery: '3 Floyds',
+  year: 2011,
+  value: 500
+},
+{
+  beerName: 'Assassin Red Wax - 1st Release',
+  brewery: 'Toppling Goliath',
+  year: 2012,
+  value: 500
+},
+{
+  beerName: '50th Anniversary',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 1998,
+  value: 500
+},
+{
+  beerName: 'Flora Cuvee',
+  brewery: 'Hill Farmstead',
+  year: 2015,
+  value: 499.99
+},
+{
+  beerName: 'Blabaer Batch 4 or 5',
+  brewery: 'Brasserie Cantillon',
+  year: 2009,
+  value: 495
+},
+{
+  beerName: 'Oude Gueuze Vintage',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2005,
+  value: 489
+},
+{
+  beerName: 'Rose de Gambrinus',
+  brewery: 'Brasserie Cantillon',
+  year: 1995,
+  value: 485
+},
+{
+  beerName: 'Oude Gueuze',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2000,
+  value: 475
+},
+{
+  beerName: 'Broken Truck',
+  brewery: 'de Garde Brewing',
+  year: 2015,
+  value: 475
+},
+{
+  beerName: 'Isabelle Proximus',
+  brewery: 'Lost Abbey',
+  year: 2008,
+  value: 475
+},
+{
+  beerName: 'Derivation #3',
+  brewery: 'Side Project',
+  year: 2015,
+  value: 470
+},
+{
+  beerName: 'Barrel Aged Imperial German Chocolate Cupcake Stout',
+  brewery: 'Angry Chair',
+  year: 2017,
+  value: 460
+},
+{
+  beerName: 'Zenne Y Frontera',
+  brewery: 'Brouerij Drie Fonteinen',
+  year: 2015,
+  value: 455
+},
+{
+  beerName: 'Utopias',
+  brewery: 'Sam Adams',
+  year: 2003,
+  value: 450
+},
+{
+  beerName: 'St Lamvinus',
+  brewery: 'Brasserie Cantillon',
+  year: 2006,
+  value: 450
+},
+{
+  beerName: 'Lou Pepe Gueuze Sticker Date 2003',
+  brewery: 'Brasserie Cantillon',
+  year: 2005,
+  value: 450
+},
+{
+  beerName: 'Anabasis',
+  brewery: 'Side Project',
+  year: 2017,
+  value: 450
+},
+{
+  beerName: 'Maple Bourbon Barrel Aged Mexican Cake',
+  brewery: 'Westbrook Brewing CO',
+  year: 2016,
+  value: 450
+},
+{
+  beerName: 'Temptation',
+  brewery: 'Russian River',
+  year: 2015,
+  value: 450
+}
+]
