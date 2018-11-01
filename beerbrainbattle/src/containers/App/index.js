0443133a1fc61d3af62151d2c9bcf02a32b63927
“Freddie <freddielev@gmail.com>
2018-11-01 15:26:35 -0600

    Begin initial file structure, add font, add logo, add beer data, dependencies, react

---
+++ beerbrainbattle/src/containers/App/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className='app-heading'>
+            <span className='heading-beer'>Beer</span>
+            <span className='heading-brain'>Brain</span>
+            <span className='heading-battle'>Battle</span>
+          </h1>
+        </header>
+        <main className='app-main'>
+          
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
