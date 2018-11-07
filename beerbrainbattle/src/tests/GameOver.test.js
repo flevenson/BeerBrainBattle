b281c96e1fbe01c50d2e604fb097ac6858490675
“Freddie <freddielev@gmail.com>
2018-11-06 18:12:29 -0700

    Write and pass mapStateToProps tests for GameOver

--- beerbrainbattle/src/tests/GameOver.test.js
+++ beerbrainbattle/src/tests/GameOver.test.js
@@ -21,4 +21,44 @@ describe('GameOver', () => {
       expect(wrapper).toMatchSnapshot()
     })
   })
+
+    describe('mapStateToProps', () => {
+    it('should parse the players from state', () => {
+      const mockState = {
+        players: mockPlayers,
+        prize: mockPrize,
+        question: mockQuestion
+      }
+      const expected = 4
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.players).toEqual(expected)
+    })
+
+    it('should parse the prize from state', () => {
+      const mockState = {
+        players: mockPlayers,
+        prize: mockPrize,
+        question: mockQuestion
+      }
+
+      const expected = mockPrize
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.prize).toEqual(expected)
+    })
+
+    it('should parse the question from state', () => {
+      const mockState = {
+        players: mockPlayers,
+        prize: mockPrize,
+        question: mockQuestion
+      }
+
+      const expected = mockQuestion
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.question).toEqual(expected)
+    })
+  })
 })
