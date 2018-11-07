6927708fa17c4f0b24e7c185992346390ed21b32
“Freddie <freddielev@gmail.com>
2018-11-06 18:09:04 -0700

    Write and pass tests for mapStateToProps for Answer

--- beerbrainbattle/src/tests/Answer.test.js
+++ beerbrainbattle/src/tests/Answer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { mockQuestion } from './testMocks.js'
+import { mockQuestion, mockPlayers } from './testMocks.js'
 import { Answer, mapStateToProps } from '../containers/Answer';
 import * as Actions from '../actions'
 
@@ -16,6 +16,31 @@ describe('Answer', () => {
   describe('Answer Component', () => {
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should parse the players from state', () => {
+      const mockState = {
+        players: mockPlayers,
+        question: mockQuestion[0]
+      }
+      const expected = 4
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.players).toEqual(expected)
+    })
+
+    it('should parse the question from state', () => {
+      const mockState = {
+        players: mockPlayers,
+        question: mockQuestion[0]
+      }
+
+      const expected = mockQuestion[0]
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.question).toEqual(expected)
     })
   })
 })
